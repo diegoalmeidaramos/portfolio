3b15ffac5b4fb7369b2df9133c5bc88439a69967
DIEGO DE ALMEIDA RAMOS <123272870+diegoalmeidaramos@users.noreply.github.com>
2024-01-17 23:45:50 -0300

    comitei index e js

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -2,7 +2,15 @@ const acordeonAcionar = document.querySelectorAll('.acordeon.acionar')
 
 acordeonAcionar.forEach((acionar)=> {
     acionar.addEventListener('click',(e) =>{
-        console.log('show')
+        const acordeon = acionar.parentElement
+        
+        const estaAberto = acordeon.classList.contains('aberto')
 
-    })
+        if (estaAberto){
+            acordeon.classList.remove('aberto')
+        }
+        else{
+            acordeon.classList.add('aberto')
+        }
+  })
 })
