5ed6452d6173d56e337aa4b50cea4fbd73fe140f
DIEGO DE ALMEIDA RAMOS <123272870+diegoalmeidaramos@users.noreply.github.com>
2024-01-17 22:28:45 -0300

    atualizei index e js

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,7 +1,7 @@
 const acordeonAcionar = document.querySelectorAll('.acordeon.acionar')
 
 acordeonAcionar.forEach((acionar)=> {
-    acionar.addEventListener('Click',(e) =>{
+    acionar.addEventListener('click',(e) =>{
         console.log('show')
 
     })
