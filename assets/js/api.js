a7088d0aa1d590b4ce7ccc1c2a7ce223cb650f94
DIEGO DE ALMEIDA RAMOS <123272870+diegoalmeidaramos@users.noreply.github.com>
2024-02-20 20:55:42 -0300

    criei o api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,5 @@
-function fetchDadosDoPerfil(){
-    
+async function BuscarDadosDoPerfil(){
+    const url = 'https://raw.githubusercontent.com/diegoalmeidaramos/portfolio/main/data/profile.json'
+    const buscar = await fetch(url) //FETCH = BUSCAR//
+    return await buscar.json()
 }
