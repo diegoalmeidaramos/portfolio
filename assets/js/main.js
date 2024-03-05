22d86dbb19c01949af2918ec6619883ed4bbc184
DIEGO DE ALMEIDA RAMOS <123272870+diegoalmeidaramos@users.noreply.github.com>
2024-03-05 15:06:21 -0300

    atualizei o json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,40 @@
+function atualizarInfoPerfil(dadosDoPerfil){
+   const foto = document.getElementById('perfil.foto')
+    foto.innerText = dadosDoPerfil.foto
+
+   const nome = document.getElementById('perfil.nome')
+    nome.innerText = dadosDoPerfil.nome
+   
+   const cargo = document.getElementById('perfil.cargo')
+    cargo.innerText = dadosDoPerfil.cargo
+   
+   const localizacao = document.getElementById('perfil.localizacao')
+    localizacao.innerText = dadosDoPerfil.localizacao
+
+   const fone = document.getElementById('perfil.fone')
+    fone.innerText = dadosDoPerfil.fone
+    fone.href = `tel:${dadosDoPerfil.fone}`
+
+   const email = document.getElementById('perfil.email')
+    email.innerText = dadosDoPerfil.email
+    email.href = `mailto:${dadosDoPerfil.email}`
+   
+
+    
+    
+    
+    
+   
+   
+
+}
+
+
+
+
 (async () => {
 
     const dadosDoPerfil = await buscarDadosDoPerfil()
-        console.log(dadosDoPerfil)
+        atualizarInfoPerfil(dadosDoPerfil)
 
 })()
